3537c08b5e36101e2cfdd26a1a35dabd79dcb92b
Pavana Venkata Krishna Vasamsetti <venkatakrishna.v@narayanagroup.com>
2022-11-02 19:39:38 +0530

    Added gateway routing dependencies and eureka client and initial setup

---
+++ src/test/java/com/routing/services/EmplyeeZuulApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routing.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmplyeeZuulApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
