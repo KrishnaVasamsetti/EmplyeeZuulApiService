9b0953bddecfb71e7bb5656b132dcbfdf41e64e7
Pavana Venkata Krishna Vasamsetti <venkatakrishna.v@narayanagroup.com>
2022-11-11 20:12:56 +0530

    Added logging for network calls

--- src/main/java/com/routing/services/EmplyeeZuulApiServiceApplication.java
+++ src/main/java/com/routing/services/EmplyeeZuulApiServiceApplication.java
@@ -2,13 +2,7 @@ package com.routing.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-
-import io.netty.handler.logging.LogLevel;
-import reactor.netty.http.client.HttpClient;
-import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -18,13 +12,4 @@ public class EmplyeeZuulApiServiceApplication {
 		SpringApplication.run(EmplyeeZuulApiServiceApplication.class, args);
 	}
 	
-//	@Bean
-//	public GlobalFilter customFilter() {
-//	    return new CustomGlobalFilter();
-//	}
-//	
-//	@Bean
-//	HttpClient httpClient() {
-//	    return HttpClient.create().wiretap("LoggingFilter", LogLevel.INFO, AdvancedByteBufFormat.TEXTUAL);
-//	}
 }
